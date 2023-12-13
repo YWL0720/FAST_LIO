519e691d3e6b806cf4ff4b9f25a219d3e9711a90
YWL0720 <1074328870@qq.com>
2023-12-13 23:47:03 +0800

    support for ouster

--- src/preprocess.cpp
+++ src/preprocess.cpp
@@ -190,6 +190,8 @@ void Preprocess::oust64_handler(const sensor_msgs::PointCloud2::ConstPtr &msg)
   pcl::PointCloud<ouster_ros::Point> pl_orig;
   pcl::fromROSMsg(*msg, pl_orig);
   int plsize = pl_orig.size();
+  pl_orig.height = 1;
+  pl_orig.width = plsize;
   pl_corn.reserve(plsize);
   pl_surf.reserve(plsize);
   if (feature_enabled)
